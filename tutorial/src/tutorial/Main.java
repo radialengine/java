5e9abc8d44c1a3fed5c3a60a10184134230add13
radialengine <lol@u>
2022-09-19 04:51:23 -0400

    Hello Github

---
+++ tutorial/src/tutorial/Main.java
@@ -0,0 +1,18 @@
+package tutorial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hello_world = 9;
+		double num2 = 5.0;
+		boolean b = true;
+		char c = 'h';
+		String str = "tim";
+		
+		int tim = hello_world;
+		
+		System.out.println(tim);
+	}
+
+}
