a5a8b076dc685a14b18f18cbd992b8d641a92cb7
radialengine <lol@u>
2022-09-19 05:12:09 -0400

    Basic operations Java - FreeCodeCamp

---
+++ basicOperations/src/basicOperations/basicOperations.java
@@ -0,0 +1,20 @@
+package basicOperations;
+
+public class basicOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 7;
+		int z = 56;
+		double sum = (double)y/(double)z;
+		double d = Math.pow(x, y);
+		
+		
+		
+		System.out.println(sum);
+		System.out.println(d);		
+		System.out.println(x);
+	}
+
+}
